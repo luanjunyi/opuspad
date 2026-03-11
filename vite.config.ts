25b2884158de714507053393bb8c680d628da1e1
Jerry Luan <xmaswillyou@gmail.com>
2026-03-10 22:26:08 -0700

    fix: add index.html to vite build input

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,12 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 5173,
     strictPort: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
 });
